3e1d1488b6af43334eaed267cc77c1f39ff2f56c
JG Brasier <jbrasier@g.harvard.edu>
2021-12-05 19:04:52 -0500

    Simple nonlinear coupled ODE

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Harvard AM205 Final course project
 
 ### Requirements
-This project utilises the DeepMoD library
+This project utilises the [DeepMoD library](https://phimal.github.io/DeePyMoD/)
 ```
 $ pip intsall deepymod
 ```
--- lotka_volterra.py
+++ lotka_volterra.py
@@ -17,7 +17,6 @@ from train import train
 from library import LibraryLotkaVoltera
 
 import torch
-from torch.autograd import grad
 from torch.utils.data import DataLoader
 from itertools import combinations
 from functools import reduce
@@ -25,7 +24,7 @@ from functools import reduce
 from scipy.integrate import odeint
 
 # Settings for reproducibility
-np.random.seed(2)
+np.random.seed(40)
 torch.manual_seed(0)
 
 # lotka voltera model
@@ -33,19 +32,35 @@ a = 1.
 b = 0.1
 c = 1.5
 d = 0.75
-def dU_dt(U, t=0):
-    """ Return the growth rate of predator and prey populations. 
-    U: [x, y] population vector
-    """
-    return np.array([ a*U[0] -   b*U[0]*U[1] ,
-                  -c*U[1] + d*b*U[0]*U[1] ])
+# def dU_dt(U, t=0):
+#     """ Return the growth rate of predator and prey populations. 
+#     U: [x, y] population vector
+#     """
+#     return np.array([ a*U[0] -   b*U[0]*U[1] ,
+#                   -c*U[1] + d*b*U[0]*U[1] ])
+# t = np.linspace(0, 15,  1000)              # time
+# U0 = np.array([10, 5])                     # initials conditions: 10 prey and 5 predator
+# U = odeint(dU_dt, U0, t)
 
-t = np.linspace(0, 15,  1000)              # time
-U0 = np.array([10, 5])                     # initials conditions: 10 prey and 5 predator
+def dU_dt(U, t):
+    return [U[1], -1*U[1] - 5*np.sin(U[0])]
+U0 = [2.5, 0.4]
+
+t = np.linspace(0, 5,  100)              # time
 U = odeint(dU_dt, U0, t)
 
-t_norm = t/np.max(t)
-U_norm = U/np.max(U)
+t_norm = t
+U_norm = U
+# noisify data
+noise = 0.1*np.random.randn(U.shape[0], U.shape[1])
+U_noise = U # + noise
+U_noise_norm = U_noise
+
+normalise = True
+if normalise:
+    t_norm = t/np.max(np.abs(t))
+    U_norm = U/np.max(np.abs(U))
+    U_noise_norm = U_noise/np.max(np.abs(U_noise))
 
 plt.figure()
 plt.plot(t_norm, U_norm[:, 0], 'r-', label='Prey')
@@ -57,10 +72,6 @@ plt.ylabel('population')
 plt.title('Evolution of predator and prey populations')
 plt.savefig("./figs/lotkavolterra_sim.png")
 
-# noisify data
-noise = 1.0*np.random.randn(U.shape[0], U.shape[1])
-U_noise = U + noise
-U_noise_norm = U_noise/np.max(U_noise)
 plt.figure()
 plt.plot(t_norm, U_noise_norm[:, 0], 'r-', label='Prey')
 plt.plot(t_norm, U_noise_norm[:, 1]  , 'b-', label='Predator')
@@ -99,10 +110,8 @@ optimizer = torch.optim.Adam(model.parameters(), betas=(0.99, 0.99), amsgrad=Tru
 
 # logdir
 logpath = r'./logs/lotkavoltera'
-if not os.path.exists(logpath):
-    os.makedirs(logpath)
 
-train(model, X, y, optimizer,sparsity_scheduler, log_dir='runs/2DAD/', split=0.8, max_iterations=100000, delta=1e-4, patience=8) 
+train(model, X, y, optimizer,sparsity_scheduler, log_dir=logpath, split=0.8, max_iterations=100000, delta=1e-4, patience=8) 
 
 print(model.sparsity_masks)
 print(model.estimator_coeffs())
